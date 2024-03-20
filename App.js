5e8d650001cc38fe0c0552d9b0d8e8822dc74afe
iaralvr <iaralanzasenai@gmail.com>
2024-03-20 09:37:35 -0300

    Inicio do Projeto

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home"
+         component={Home} />
+      </Tab.Navigator>
+    </NavigationContainer>
+     
+    );
+  
 }
 
 const styles = StyleSheet.create({
